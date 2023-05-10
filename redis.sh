bb5b3c5f4c5d6e71ac8b83bd6a9791868a88f88b
Manjula <manjula.niki6@gmail.com>
2023-05-11 05:29:25 +0530

    redis service

--- redis.sh
+++ redis.sh
@@ -8,6 +8,7 @@ yum install redis -y
 echo -e "\e[36m>>>> update Redis listen Address <<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0'
 
+
 echo -e "\e[36m>>>> restart the redis service <<<<\e[0m"
 systemctl enable redis
 systemctl start redis
