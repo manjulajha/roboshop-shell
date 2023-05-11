9381a1c8fcb6be0300003b2091370bff0dc58b8f
Manjula <manjula.niki6@gmail.com>
2023-05-11 06:30:36 +0530

    user add

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>> disable mysql <<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>> copy mysql repo file <<<<\e[0m"
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>> install mysql <<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>> restart <<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m>>>> reset mysql <<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
