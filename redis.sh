192159933c147e816600d485d91cc41479d8dad1
Manjula <manjula.niki6@gmail.com>
2023-05-11 05:35:06 +0530

    redis service add

--- redis.sh
+++ redis.sh
@@ -9,6 +9,7 @@ echo -e "\e[36m>>>> update Redis listen Address <<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf
 
 
+
 echo -e "\e[36m>>>> restart the redis service <<<<\e[0m"
 systemctl enable redis
 systemctl start redis
