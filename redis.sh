de5b0f8d2b16326f1171c67595eb425b71e8b268
Manjula <manjula.niki6@gmail.com>
2023-05-11 05:34:25 +0530

    redis service

--- redis.sh
+++ redis.sh
@@ -6,7 +6,7 @@ dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
 echo -e "\e[36m>>>> update Redis listen Address <<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0'
+sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf
 
 
 echo -e "\e[36m>>>> restart the redis service <<<<\e[0m"
